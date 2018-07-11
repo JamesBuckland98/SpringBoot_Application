ba3894e43d2a69c3f96490ff7f7ac1a2a947d878
Carl Jones <jonesc162@cardiff.ac.uk>
2018-07-11 14:26:51 +0100

    Setup initial project.
    Gradle dependencies commented out for now.
    
    Just a static page that has been manually tested.

---
+++ src/main/java/com/nsa/cm6213/example/charity2018/Charity2018Application.java
@@ -0,0 +1,12 @@
+package com.nsa.cm6213.example.charity2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Charity2018Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Charity2018Application.class, args);
+    }
+}
---
+++ src/test/java/com/nsa/cm6213/example/charity2018/Charity2018ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nsa.cm6213.example.charity2018;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Charity2018ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
