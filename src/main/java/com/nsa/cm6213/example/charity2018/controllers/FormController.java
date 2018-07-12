7bf5dbaf507e06d319b04f7b66ae85d9182caeb2
Carl Jones <jonesc162@cardiff.ac.uk>
2018-07-12 11:05:32 +0100

    Added a singleton class to play the role of a CharityService.
    We don't want this type of responsibility in the controller.
    
    Later on, we will replace this singleton with a Spring Bean.
    
    Note the use of an interface.  This will allow the controller to easily replace the singleton.
    
    The results of the call to the CharityService are added to the model.  This "model" is part of the MVC (model-view-controller) pattern implementation in Spring.  It is the part of the domain that the next web screen requires to render itself.  The model consists of a set of key-value pairs that we pass back to the view.  We return the name of the view.  By convention, this is just the name of the HTML file in the templates folder.
    
    The template uses the Thymeleaf template engine.  Thymeleaf can access the fields by key.  An example of iteration is shown as well as some in-page display logic.

--- src/main/java/com/nsa/cm6213/example/charity2018/controllers/FormController.java
+++ src/main/java/com/nsa/cm6213/example/charity2018/controllers/FormController.java
@@ -1,16 +1,33 @@
 package com.nsa.cm6213.example.charity2018.controllers;
 
+import com.nsa.cm6213.example.charity2018.domain.Charity;
+import com.nsa.cm6213.example.charity2018.services.CharityService;
+import com.nsa.cm6213.example.charity2018.services.CharityServiceStatic;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 //This is a controller.  It is NOT a RestController.  It is part of the Spring MVC framework so more aligned with server-side generation of HTML.
 public class FormController {
+
     @RequestMapping(path = "/findCharity", method = RequestMethod.POST) //handles a form POST to /name
     public String postName(@ModelAttribute CharityForm in_form, Model model) { //ModelAttribute maps the form to an object
+
+        CharityService charityService = CharityServiceStatic.getInstance();
+
+        List<Charity> matchingCharities = charityService.findCharities(in_form.getName());
+
         System.out.println("name equals " + in_form.getName());//we can access the form as a normal object
-        return "redirect:/home.html"; //the return is the name of the next page (but we're not doing templating, so we redirect to the home page.
+        System.out.println("matching charity = " + matchingCharities);
+
+        model.addAttribute("searchTerm", in_form.getName());
+        model.addAttribute("matches", matchingCharities);
+
+
+        return "charitySearchResults"; //the return is the name of the next page (but we're not doing templating, so we redirect to the home page.
     }
 
     /*
---
+++ src/main/java/com/nsa/cm6213/example/charity2018/services/CharityServiceStatic.java
@@ -0,0 +1,38 @@
+package com.nsa.cm6213.example.charity2018.services;
+
+import com.nsa.cm6213.example.charity2018.domain.Charity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CharityServiceStatic implements CharityService {
+
+    private static CharityServiceStatic theInstance;
+    private List<Charity> theCharities;
+
+    public static CharityService getInstance(){
+        if (theInstance == null){
+            theInstance = new CharityServiceStatic();
+        }
+        return theInstance;
+    }
+
+    private CharityServiceStatic(){
+
+        theCharities = new ArrayList<Charity>();
+        theCharities.add(new Charity("NSPCC","Kids charity","nspcc", "12345678", true));
+        theCharities.add(new Charity("RSPCA","Animal charity","rspca", "23456789", true));
+        theCharities.add(new Charity("BHF","Heart charity","bhf", "34567890", true));
+        theCharities.add(new Charity("Tenovus","Cancer charity","tenovus", "56789012", true));
+        theCharities.add(new Charity("Oxfam","Famine charity","oxfam", "98765432", true));
+    }
+
+    public List<Charity> findCharities(String searchTerm){
+        return theCharities
+                .stream()
+                .filter(c -> c.getName().equals(searchTerm))
+                .collect(Collectors.toList());
+    }
+
+}
