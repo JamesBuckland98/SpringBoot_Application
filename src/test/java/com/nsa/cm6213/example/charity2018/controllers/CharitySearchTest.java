450f1d4a0c6301074b089a5cfa6399e7fd11f6f7
Carl Jones <jonesc162@cardiff.ac.uk>
2018-07-12 11:54:14 +0100

    Added automated tests using Spring's built-in support.
    These tests DO NOT start a server, but test within the application context.
    
    This makes them quicker to run.  They run as part of the build.
    
    There should be a lot more tests here, including negative tests (search for CRUK and return nothing).
    
    Writing these tests first will force the application to update itself to pass these tests.
    
    We will do this next.

---
+++ src/test/java/com/nsa/cm6213/example/charity2018/controllers/CharitySearchTest.java
@@ -0,0 +1,66 @@
+package com.nsa.cm6213.example.charity2018.controllers;
+
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.nsa.cm6213.example.charity2018.domain.Charity;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * @see https://spring.io/guides/gs/testing-web/
+ */
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CharitySearchTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnHomePage() throws Exception {
+        this.mockMvc.perform(get("/home.html")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Welcome to Charity Giving")));
+    }
+
+    @Test
+    public void shouldFindNSPCC() throws Exception {
+
+        Charity nspcc = new Charity("NSPCC", "Kids charity", "nspcc", "12345678", true);
+
+        mockMvc.perform(post("/findCharity")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("name", "NSPCC")
+
+        )
+                .andExpect(status().isOk())
+                .andExpect(view().name("charitySearchResults"))
+                .andExpect(model().attribute("searchTerm", "NSPCC"))
+                .andExpect(model().attribute("matches", hasItem(nspcc)))
+                .andExpect(content().string(stringContainsInOrder(Arrays.asList("<div>","<span>NSPCC</span>","<span>Kids charity</span>","<span>12345678</span>","</div>"))));
+                ;
+
+
+
+    }
+
+
+}
+
+
